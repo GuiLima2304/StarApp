364cd92eaf63363759a7be9280ab7076d1e25c9c
Guilherme Lima <gsouza@luxfacta.com>
2019-05-31 14:53:23 -0300

    90% concluido

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,14 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  animations: [trigger('fadeInOut', [  state('void', style({    opacity: 0  })),  transition('void <=> *', animate(3000)),])],
+  styleUrls: ['./home.component.scss'],
+  
 })
 export class HomeComponent implements OnInit {
 
   constructor() { }
-
+  
   ngOnInit() {
   }
 
